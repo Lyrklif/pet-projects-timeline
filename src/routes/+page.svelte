4edf3f5a593bd190a31e7ea728dde674ad6dd677
Alexandr Kochurov <b2b4ly@gmail.com>
2024-10-18 21:32:44 +0400

    style: run eslint & prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,26 @@
 <script>
-	import { Alert } from 'flowbite-svelte';
-	import { Heading, P, Button } from 'flowbite-svelte';
+	import { Heading } from 'flowbite-svelte';
+	import { useQuery } from '@sveltestack/svelte-query';
 
-
-	import { useQuery } from '@sveltestack/svelte-query'
-
-	const DAY = 1000 * 60 * 60 * 24
+	const DAY = 1000 * 60 * 60 * 24;
 
 	const queryProfile = useQuery({
 		queryKey: ['user-profile'],
-		queryFn: () => fetch(
-			'https://api.github.com/repos/SvelteStack/svelte-query'
-		).then(res => res.json()),
+		queryFn: () =>
+			fetch('https://api.github.com/repos/SvelteStack/svelte-query').then((res) => res.json()),
 		cacheTime: DAY,
 		staleTime: DAY,
 		select: (context) => context || {}
-	})
+	});
 </script>
-
 
 <svelte:head>
 	<title>Pet projects</title>
 	<meta name="description" content="Timeline of pet projects" />
 </svelte:head>
 
-
 <header>
-	<Heading tag="h1" class="mb-4">
-		Pet projects
-	</Heading>
-
-
-	<Alert>
-		<span class="font-medium">Info alert!</span>
-		Change a few things up and try submitting again.
-	</Alert>
+	<Heading tag="h1" class="mb-4">Pet projects</Heading>
 
 	{#if $queryProfile.isLoading}
 		Loading...
@@ -53,7 +39,7 @@
 </header>
 
 <section>
-	<h2>Timeline</h2>
+	<Heading tag="h2">Timeline</Heading>
 
 	<ul>
 		<li>Project 1</li>
