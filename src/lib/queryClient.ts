a2c6d6248c15b2b4e6123d5c08a4ccd302cf0692
Alexandr Kochurov <b2b4ly@gmail.com>
2024-10-20 03:15:17 +0400

    Updates

--- src/lib/queryClient.ts
+++
@@ -1,5 +0,0 @@
-import { QueryClient } from '@sveltestack/svelte-query';
-
-const queryClient = new QueryClient();
-
-export default queryClient;
--- src/stores/date.ts
+++
@@ -1,5 +0,0 @@
-export function formatDate(date) {
-	const options = { day: '2-digit', month: 'long', year: 'numeric' };
-
-	return new Intl.DateTimeFormat('en-EN', options).format(new Date(date));
-}
