8a29bccfda99f957d642fb23836df0efb93d0016
Alexandr Kochurov <b2b4ly@gmail.com>
2024-10-20 02:06:00 +0400

    feat: delete unused api

--- src/constants/query.ts
+++ src/constants/query.ts
@@ -4,7 +4,6 @@ export const API_PATH = import.meta.env.VITE_API;
 const userName = import.meta.env.VITE_USER_NAME;
 
 export const API = {
-	USER: `${API_PATH}/users/${userName}`,
 	REPOSITORIES: `${API_PATH}/users/${userName}/repos`,
 	REPOSITORY: (name: string) => `${API_PATH}/repos/${userName}/${name}`,
 	REPO_README: (name: string) => `${API_PATH}/repos/${userName}/${name}/readme`
